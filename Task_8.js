c792211ed1e789f640dd9854efb57d82b7de57c1
AdelZinnurova <ad187174@bk.ru>
2024-10-31 12:28:57 +0400

    task8

---
+++ Task_8.js
@@ -0,0 +1,16 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Очень просто: задано число (целое / десятичное / оба в зависимости от языка), найдите его противоположность (аддитивную обратную величину).
+
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+
+function opposite(number) {
+    return -number;
+  }
